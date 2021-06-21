2f77294212fe48d2ec017c2c36588fee9dca5d3c
sdpercy <scottpercy@hotmail.com>
2021-06-21 15:27:46 -0230

    added animals

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
@@ -99,6 +101,22 @@ app.get('/api/animals', (req, res) => {
       } else {
         res.send(404);
       }
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
   app.post('/api/animals', (req, res) => {
